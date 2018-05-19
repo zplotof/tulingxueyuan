033c72f73eb5bd6d2e8d36c45099ec288fc7a2de
zplotof <zplotof@gmail.com>
2018-05-19 23:00:02 +0800

    12

---
+++ OOP/01.py
@@ -0,0 +1,33 @@
+'''
+定义一个学生类, 用来形容学生
+'''
+#定义一个空的类
+class Student():
+    pass
+
+#定义一个对象
+mingyue = Student()
+
+
+#
+class PythonStudent():
+    #用none给不确定的值占位
+    name = None
+    age = 18
+    course = "Python"
+    
+    def doHomework(self):
+        print ("i am doing my homework")
+        #推荐在函数末尾使用return语句
+        return None
+
+yueyue = PythonStudent()
+print (yueyue.age)
+print (yueyue.name)
+yueyue.doHomework()
+
+
+
+
+    
+        
---
+++ testwendang.py
@@ -0,0 +1,11 @@
+def stu(name,age):
+    '''
+
+    :param name: 1
+    :param age: 2
+    :return: 3
+    '''
+    pass
+print(help(stu))
+print ("*" * 20)
+print (stu.__doc__)
